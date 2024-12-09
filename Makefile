4e10cb5db8c0c9e94d23eb4648aa17ac92202543
Gustav Lindblom <contact@gustavlindblom.se>
2024-12-09 11:06:33 +0100

    [day2] boilerplate

--- Makefile
+++ Makefile
@@ -1,3 +1,7 @@
 day1: src/day1.c
 	mkdir -p build
 	cc -ggdb -Wall -Wextra -o build/day1 src/day1.c
+
+day2: src/day2.c
+	mkdir -p build
+	cc -ggdb -Wall -Wextra -o build/day2 src/day2.c
---
+++ src/day2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+#define EXAMPLE "7 6 4 2 1\n" \
+"1 2 7 8 9\n"                 \
+"9 7 6 2 1\n"                 \
+"1 3 2 4 5\n"                 \
+"8 6 4 4 1\n"                 \
+"1 3 6 7 9"                 
+
+int main(void) {
+  printf("Hello world\n");
+  return 0;
+}
