5b81e70f5fd8d5c679a9b604093d143c9cb73cfa
Gustav Lindblom <contact@gustavlindblom.se>
2024-12-10 12:57:40 +0100

    [day4] solve part 2

--- src/day4.c
+++ src/day4.c
@@ -85,10 +85,83 @@ void solve_part_1(char *input) {
   printf("words found: %d\n", words_found);
 }
 
+void solve_part_2(char *input) {
+  int rows, columns;
+  count_rows_and_columns(input, &rows, &columns);
+  char word_search[rows][columns];
+  bool grid_usage[rows][columns];
+  char word_search_solved[rows][columns];
+  
+  for (int y = 0; y < rows; y++) {
+    for (int x = 0; x < columns; x++) {
+      int idx = (y * rows) + x /* skip newline characters */ + (1 * y); 
+      word_search[y][x] = input[idx];
+      word_search_solved[y][x] = '.';
+    }
+  }
+  int words_found = 0;
+  for (int y = 0; y < rows; y++) {
+    for (int x = 0; x < columns; x++) {
+      char current = word_search[y][x];
+      if ((current == 'M' || current == 'S') && !grid_usage[y][x]) {
+        char next = 'A';
+        char last = current == 'M' ? 'S' : 'M';
+        if (y - 1 > 0 && x - 1 > 0 && word_search[y-1][x-1] == next && !grid_usage[y-1][x-1]) {
+          if (y - 2 > 0 && x - 2 > 0 && word_search[y-2][x-2] == last) {
+            char dC = word_search[y][x-2];
+            if (dC == 'M' || dC == 'S') {
+              char dL = dC == 'M' ? 'S' : 'M';
+              if (word_search[y-2][x] == dL) {
+                words_found++;
+                word_search_solved[y][x] = current;
+                word_search_solved[y-1][x-1] = 'A';
+                word_search_solved[y-2][x-2] = last;
+                word_search_solved[y][x-2] = dC;
+                word_search_solved[y-2][x] = dL;
+                grid_usage[y][x] = true;
+                grid_usage[y-1][x-1] = true;
+                grid_usage[y-2][x-2] = true;
+              }
+            }
+          }
+        }
+        if (y + 1 < rows && x + 1 < columns && word_search[y+1][x+1] == next && !grid_usage[y+1][x+1]) {
+          if (y + 2 < rows && x + 2 < columns && word_search[y+2][x+2] == last) {
+            char dC = word_search[y][x+2];
+            if (dC == 'M' || dC == 'S') {
+              char dL = dC == 'M' ? 'S' : 'M';
+              if (word_search[y+2][x] == dL) {
+                words_found++;
+                word_search_solved[y][x] = current;
+                word_search_solved[y+1][x+1] = 'A';
+                word_search_solved[y+2][x+2] = last;
+                word_search_solved[y][x+2] = dC;
+                word_search_solved[y+2][x] = dL;
+                grid_usage[y][x] = true;
+                grid_usage[y+1][x+1] = true;
+                grid_usage[y+2][x+2] = true;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  for (int y = 0; y < rows; y++) {
+    for (int x = 0; x < columns; x++) {
+      printf("%c", word_search_solved[y][x]);
+    }
+    printf("\n");
+  }
+  printf("X-MAS found: %d\n", words_found);
+}
+
 int main(void) {
   char *input = read_file("src/input4.txt");
+  // char *example = read_file("src/example4.txt");
   printf("=== PART 1 ===\n");
   solve_part_1(input);
   printf("\n=== PART 2 ===\n");
+  solve_part_2(input);
   return 0;
 }
